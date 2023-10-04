5f65da9af6cf1847eb95a50a500898040f9d62a7
Ac Hybl <matoush@southern.edu>
2023-10-04 19:23:02 +0000

    tried lmqg training. failed

--- lmqg/lmqgTrain.py
+++ lmqg/lmqgTrain.py
@@ -1,8 +1,13 @@
 from lmqg import GridSearcher
+
+local_model_location = "/home/ac/code/aqg/models/llama-hf/7b"
+local_output_location = "/home/ac/code/aqg/models/output/7bTrainedWithLmqg"
+model_id = 'meta-llama/Llama-2-7b-hf'
+
 trainer = GridSearcher(
-    checkpoint_dir='../models/llama-hf/7b',
+    checkpoint_dir=local_output_location,
     dataset_path='lmqg/qg_squad',
-    model='t5-small',
+    model=local_model_location,
     epoch=15,
     epoch_partial=5,
     batch=64,
