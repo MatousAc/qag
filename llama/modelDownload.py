d69609d24ff7e359f536cfea12669296c47a5187
Ac Hybl <matoush@southern.edu>
2023-10-09 02:14:09 +0000

    model download script working

--- llama/modelDownload.py
+++ llama/modelDownload.py
@@ -2,7 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import transformers
 
 model_source = input('hf model name: ')
-model_destination = f'/home/ac/code/PBE_AQG/models/{input("save to (models/...): ")}/'
+model_destination = f'/home/ac/code/qag/models/{input("save to (models/...): ")}/'
 
 model = AutoModelForCausalLM.from_pretrained(model_source)
 tokenizer = AutoTokenizer.from_pretrained(model_source)
