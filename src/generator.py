60e1f1af260fee7341bb3de2b9b11ba0ca5b7d06
MatousAc <matoush@southern.edu>
2025-08-02 21:43:06 -0400

    misc

--- src/generator.py
+++ src/generator.py
@@ -221,6 +221,7 @@ if __name__ == '__main__':
     texts = input("Enter references for qag in a comma-separated list: ")
     texts = texts.split(', ')
     generator.refList = generator.dp.enumerateContext(texts)
+  print(generator.refList)
   
   # main function execution
   if 'interactive' in args: generator.testGen()
